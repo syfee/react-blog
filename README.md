78411f871710c2f4fc5855d9e4ee790a629aa2f3
shaoyangfeng <shaoyangfeng@meituan.com>
2023-12-10 20:55:46 +0800

    cleanup for nextjs

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-blog
-It's a web app based on React and Node, will use library like Reason, GraphQL, Rxjs, Ramda... The main purpose of this app is "Learning by doing".
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), mdx()]
-})
